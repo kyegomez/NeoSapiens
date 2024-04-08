b7577ed9b553f25a836ca624049ef748db71770a
Kye <kye@apacmediasolutions.com>
2024-04-08 16:43:24 -0400

    [CLEANUP]

---
+++ neo_sapiens/few_shot_prompts.py
@@ -0,0 +1,127 @@
+
+# Example usage
+data = """
+{
+    "plan": ["Step 1", "Step 2", "Step 3"],
+    "number_of_agents": 5,
+    "agents": [
+        {
+            "name": "Agent 1",
+            "system_prompt": "Prompt 1"
+        },
+        {
+            "name": "Agent 2",
+            "system_prompt": "Prompt 2"
+        }
+    ]
+}
+"""
+
+# AI Research Team 1
+data1 = """
+{
+    "plan": ["Data Collection", "Data Cleaning", "Model Training", "Model Evaluation"],
+    "number_of_agents": 3,
+    "agents": [
+        {
+            "name": "Data Agent",
+            "system_prompt": "Collect and clean data"
+        },
+        {
+            "name": "Training Agent",
+            "system_prompt": "Train the model"
+        },
+        {
+            "name": "Evaluation Agent",
+            "system_prompt": "Evaluate the model"
+        }
+    ]
+}
+"""
+
+# AI Research Team 2
+data2 = """
+{
+    "plan": ["Literature Review", "Hypothesis Formulation", "Experiment Design", "Data Analysis", "Paper Writing"],
+    "number_of_agents": 5,
+    "agents": [
+        {
+            "name": "Review Agent",
+            "system_prompt": "Review the literature"
+        },
+        {
+            "name": "Hypothesis Agent",
+            "system_prompt": "Formulate the hypothesis"
+        },
+        {
+            "name": "Design Agent",
+            "system_prompt": "Design the experiment"
+        },
+        {
+            "name": "Analysis Agent",
+            "system_prompt": "Analyze the data"
+        },
+        {
+            "name": "Writing Agent",
+            "system_prompt": "Write the paper"
+        }
+    ]
+}
+"""
+
+# AI Research Team 3
+data3 = """
+{
+    "plan": ["Problem Identification", "Solution Design", "Implementation", "Testing", "Deployment"],
+    "number_of_agents": 4,
+    "agents": [
+        {
+            "name": "Identification Agent",
+            "system_prompt": "Identify the problem"
+        },
+        {
+            "name": "Design Agent",
+            "system_prompt": "Design the solution"
+        },
+        {
+            "name": "Implementation Agent",
+            "system_prompt": "Implement the solution"
+        },
+        {
+            "name": "Deployment Agent",
+            "system_prompt": "Deploy the solution"
+        }
+    ]
+}
+"""
+
+
+data5 = """
+{
+    "plan": ["Room Management", "Guest Services", "Reservations Handling", "Facility Maintenance", "Staff Coordination"],
+    "number_of_agents": 5,
+    "agents": [
+        {
+            "name": "Room Management Agent",
+            "system_prompt": "Automate room assignments, minibar restocking, and housekeeping schedules"
+        },
+        {
+            "name": "Guest Services Agent",
+            "system_prompt": "Handle check-ins, check-outs, guest requests, and complaints efficiently"
+            "tool": "browser"
+        },
+        {
+            "name": "Reservations Agent",
+            "system_prompt": "Manage room bookings, table reservations, and special requests"
+        },
+        {
+            "name": "Maintenance Agent",
+            "system_prompt": "Schedule and track maintenance tasks for facilities and rooms"
+        },
+        {
+            "name": "Staff Coordination Agent",
+            "system_prompt": "Optimize staff schedules, task assignments, and workload distribution"
+        }
+    ]
+}
+"""
--- neo_sapiens/hass_schema.py
+++ neo_sapiens/hass_schema.py
@@ -1,8 +1,17 @@
-from pydantic import BaseModel, Field
 from typing import List
-from swarms.utils.json_utils import base_model_to_json
+
+from pydantic import BaseModel, Field
 from swarms import Agent, OpenAIChat
-from neo_sapiens.main import terminal, browser
+from swarms.utils.json_utils import base_model_to_json
+
+from neo_sapiens.hass_schema import (
+    data,
+    data1,
+    data2,
+    data3,
+    data5,
+)
+from neo_sapiens.main import browser, terminal
 
 
 def tool_router(tool: str, *args, **kwargs):
@@ -60,11 +69,18 @@ class HassSchema(BaseModel):
         title="List of agents to use for the problem",
         description="List of agents to use for the problem",
     )
+    # Rules for the agents
+    rules: str = Field(
+        ...,
+        title="Rules for the agents",
+        description="Rules for the agents",
+    )
 
 
-json = HassSchema.model_json_schema()
-json = base_model_to_json(HassSchema)
-print(f"JSON Schema: {json}")
+def transform_schema_to_json(schema: BaseModel):
+    json = schema.model_json_schema()
+    json = base_model_to_json(HassSchema)
+    print(f"JSON Schema: {json}")
 
 
 def parse_hass_schema(data: str) -> tuple:
@@ -87,132 +103,6 @@ def parse_hass_schema(data: str) -> tuple:
     )
 
 
-# Example usage
-data = """
-{
-    "plan": ["Step 1", "Step 2", "Step 3"],
-    "number_of_agents": 5,
-    "agents": [
-        {
-            "name": "Agent 1",
-            "system_prompt": "Prompt 1"
-        },
-        {
-            "name": "Agent 2",
-            "system_prompt": "Prompt 2"
-        }
-    ]
-}
-"""
-
-# AI Research Team 1
-data1 = """
-{
-    "plan": ["Data Collection", "Data Cleaning", "Model Training", "Model Evaluation"],
-    "number_of_agents": 3,
-    "agents": [
-        {
-            "name": "Data Agent",
-            "system_prompt": "Collect and clean data"
-        },
-        {
-            "name": "Training Agent",
-            "system_prompt": "Train the model"
-        },
-        {
-            "name": "Evaluation Agent",
-            "system_prompt": "Evaluate the model"
-        }
-    ]
-}
-"""
-
-# AI Research Team 2
-data2 = """
-{
-    "plan": ["Literature Review", "Hypothesis Formulation", "Experiment Design", "Data Analysis", "Paper Writing"],
-    "number_of_agents": 5,
-    "agents": [
-        {
-            "name": "Review Agent",
-            "system_prompt": "Review the literature"
-        },
-        {
-            "name": "Hypothesis Agent",
-            "system_prompt": "Formulate the hypothesis"
-        },
-        {
-            "name": "Design Agent",
-            "system_prompt": "Design the experiment"
-        },
-        {
-            "name": "Analysis Agent",
-            "system_prompt": "Analyze the data"
-        },
-        {
-            "name": "Writing Agent",
-            "system_prompt": "Write the paper"
-        }
-    ]
-}
-"""
-
-# AI Research Team 3
-data3 = """
-{
-    "plan": ["Problem Identification", "Solution Design", "Implementation", "Testing", "Deployment"],
-    "number_of_agents": 4,
-    "agents": [
-        {
-            "name": "Identification Agent",
-            "system_prompt": "Identify the problem"
-        },
-        {
-            "name": "Design Agent",
-            "system_prompt": "Design the solution"
-        },
-        {
-            "name": "Implementation Agent",
-            "system_prompt": "Implement the solution"
-        },
-        {
-            "name": "Deployment Agent",
-            "system_prompt": "Deploy the solution"
-        }
-    ]
-}
-"""
-
-
-data5 = """
-{
-    "plan": ["Room Management", "Guest Services", "Reservations Handling", "Facility Maintenance", "Staff Coordination"],
-    "number_of_agents": 5,
-    "agents": [
-        {
-            "name": "Room Management Agent",
-            "system_prompt": "Automate room assignments, minibar restocking, and housekeeping schedules"
-        },
-        {
-            "name": "Guest Services Agent",
-            "system_prompt": "Handle check-ins, check-outs, guest requests, and complaints efficiently"
-            "tool": "browser"
-        },
-        {
-            "name": "Reservations Agent",
-            "system_prompt": "Manage room bookings, table reservations, and special requests"
-        },
-        {
-            "name": "Maintenance Agent",
-            "system_prompt": "Schedule and track maintenance tasks for facilities and rooms"
-        },
-        {
-            "name": "Staff Coordination Agent",
-            "system_prompt": "Optimize staff schedules, task assignments, and workload distribution"
-        }
-    ]
-}
-"""
 
 
 def merge_plans_into_str(
@@ -259,9 +149,9 @@ def create_agents(
             agent_name=name,
             system_prompt=system_prompt,
             llm=OpenAIChat(
-                openai_api_key="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",
+                openai_api_key=None,
             ),
-            max_loops=1,
+            max_loops="auto",
             autosave=True,
             dashboard=False,
             verbose=True,
