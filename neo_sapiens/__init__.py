b755499aa1fd5b3336dc5ddc9ac2f35131a32370
Kye <kye@apacmediasolutions.com>
2024-04-09 11:10:14 -0400

    [CLEANUP]

--- neo_sapiens/__init__.py
+++ neo_sapiens/__init__.py
@@ -5,7 +5,7 @@ from neo_sapiens.few_shot_prompts import (
     data3,
     data5,
 )
-from hass_schema import (
+from neo_sapiens.hass_schema import (
     parse_hass_schema,
     merge_plans_into_str,
     merge_rules_into_str,
